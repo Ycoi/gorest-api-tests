e641de207dae41e85845c47afd87d02141b8223d
Jukka Koivunen <jjkoivun@gmail.com>
2024-06-26 16:48:46 +0200

    Add first tests for user POST and read the authentication token from .env file

--- user.test.ts
+++ user.test.ts
@@ -1,9 +1,13 @@
 
 import request from "supertest";
 
+import dotenv from "dotenv";
+
+dotenv.config();  // Load environment variables from .env file
+
 const baseUrl = 'https://gorest.co.in/public/v2/';
 
-const TOKEN = 'd3f371aaa034260691246bd2b4f338b11de4c3d11043a4cdf4a088eca089556f';
+const TOKEN = process.env.GOREST_BEARER_TOKEN;
 
 describe('GET users', () => {
 	it('should return a 200 status code when unauthenticated', async () => {
@@ -21,7 +25,7 @@ describe('GET users', () => {
 	});
     it('should return an individual user for valid user id', async () => {
 		const response = await request(baseUrl)
-			.get('users/6940140')
+			.get('users/6940141')
             .set('Authorization', `Bearer ${TOKEN}`);
 
 		expect(response.statusCode).toBe(200);
@@ -32,5 +36,21 @@ describe('GET users', () => {
             .set('Authorization', `Bearer ${TOKEN}`);
 
 		expect(response.statusCode).toBe(404);
+	});
+});
+
+describe('POST users', () => {
+	it('should return a 401 status code when unauthenticated', async () => {
+		const response = await request(baseUrl)
+			.post('users');
+
+		expect(response.statusCode).toBe(401);
+	});
+    it('should return a 200 status code when authenticated', async () => {
+		const response = await request(baseUrl)
+			.get('users')
+            .set('Authorization', `Bearer ${TOKEN}`);
+
+		expect(response.statusCode).toBe(200);
 	});
 });
